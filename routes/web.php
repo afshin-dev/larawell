06fb71f92ae01557f1f8110b664eb10233e291bb
afshin <shahinof10009@gmail.com>
2023-06-05 17:10:32 +0330

    passing data to view - abort_if()

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,17 @@ Route::get("/users", function () {
     return view("users.home");
 })->name("users.home")
 ;
+
+Route::get("/posts/{id}", function($id) {
+    $posts = [
+        ["title" => "t1" , "desc" => "desc1" ],
+        ["title" => "t2" , "desc" => "desc2" ],
+    ];
+
+    abort_if(!isset($posts[$id]), 404);
+
+
+
+    $post = array_key_exists($id, $posts) ? $posts[$id] : [] ;  
+    return view("posts.details", ["post" =>$post ]);
+})->name("posts.details");
